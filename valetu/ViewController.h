fabb182dd4cdbd0733a39a75bb802e794b469dc7
imobile <imobile@imobiles-mac.local>
2016-09-05 07:46:24 -0700

    Initial Commit

---
+++ valetu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  valetu
+//
+//  Created by imobile on 2016-09-05.
+//  Copyright © 2016 imobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ valetu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  valetu
+//
+//  Created by imobile on 2016-09-05.
+//  Copyright © 2016 imobile. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
